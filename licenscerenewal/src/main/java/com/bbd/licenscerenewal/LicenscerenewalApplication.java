f95140f3ac350e9e818ee7e668c8b2ad925180bd
Callum <callum.m.dunne@gmail.com>
2020-08-08 14:16:29 +0200

    gitflow-feature-stash: CreatingProject

---
+++ licenscerenewal/src/main/java/com/bbd/licenscerenewal/LicenscerenewalApplication.java
@@ -0,0 +1,13 @@
+package com.bbd.licenscerenewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenscerenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenscerenewalApplication.class, args);
+	}
+
+}
