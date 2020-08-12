8519035a0bb9c424b1a543e0c9a650b58e587d52
Emma Coetzer <emmac@bbd.co.za>
2020-08-12 18:11:23 +0200

    Added endpoint comments

--- licenscerenewal/src/main/java/com/bbd/licenscerenewal/controllers/Index.java
+++ licenscerenewal/src/main/java/com/bbd/licenscerenewal/controllers/Index.java
@@ -16,4 +16,32 @@ public class Index {
         return "test";
     }
 
+    //API ENPOINTS TO CREATE
+
+    //Get all Licenses
+    //Get license ready for renewal based on dates passed
+    //Get license based on license number
+    //Get license by expiry date
+    //Get license by date issued
+    //Get license by owner
+    //Get licenses for organisation
+    //Get license based on vehicle parameters
+    //Get licenses based on status
+    //Get licenses based on type
+    //Post new license
+    //Update license and everything that comes with that
+
+    //Get all vehicles
+    //Get vehicle based on owner
+    //Get vehicle based on license 
+    //Get vehicle based on vin
+    //Get vehicle based on make
+    //Get vehicle by type
+    //Post register vehicle ???? NOT SURE ABOUT THIS
+
+    //Get license renewal history based on license
+    //Get all renewal dates for license
+    //Get fee for license renewal
+    //Post pay for license
+
 }
