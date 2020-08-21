4ae433f47e9d926f92ae2093e14878bf2e3f4e74
Marcelle <marcelleb@bbd.co.za>
2020-08-22 00:08:33 +0200

    Added Exception Handling

---
+++ licenscerenewal/src/main/java/com/bbd/licenscerenewal/exceptionHandling/CustomExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.bbd.licenscerenewal.exceptionHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+   
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorMessage> getRuntimeExceptionData(RuntimeException exception) {
+        ErrorMessage error = new ErrorMessage(500, "Something has gone wrong. Please contact you service provider");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ErrorMessage> getHttpClientErrorData(HttpClientErrorException ex) {
+        ErrorMessage error;
+        switch (ex.getRawStatusCode()) {
+            case 400:
+                error = new ErrorMessage(400, "Bad request");
+                return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+            case 401:
+                error = new ErrorMessage(401, "Unauthorized");
+                return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+            case 404:
+                error = new ErrorMessage(404, "Cannot be found");
+                return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+            case 405:
+                error = new ErrorMessage(405, "Method not allowed");
+                return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
+            case 422:
+                error = new ErrorMessage(422, "Unprocessable Entity");
+                return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<ErrorMessage> getHttpServerError(HttpServerErrorException ex){
+        ErrorMessage error = new ErrorMessage(ex.getRawStatusCode(), "Something has gone wrong. Please contact you service provider");
+        return new ResponseEntity<>(error, ex.getStatusCode());
+    }
+
+}
